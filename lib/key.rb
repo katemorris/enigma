ff7d611486a7c69f85f8fc4f05bb80739ddabb9e
Kate Tester <katemorris@gmail.com>
2020-09-18 22:38:52 -0600

    Adds breakdown method for key.

--- lib/key.rb
+++ lib/key.rb
@@ -1,6 +1,15 @@
 class Key
-  attr_reader :value
-  def initialize
-    @value = 5.times.map { rand(10) }.join
+  def value
+    5.times.map { rand(10) }.join
+  end
+
+  def breakdown
+    key_split = value.to_s.split("")
+    list = {}
+    list[:A] = (key_split[0] + key_split[1]).to_i
+    list[:B] = (key_split[1] + key_split[2]).to_i
+    list[:C] = (key_split[2] + key_split[3]).to_i
+    list[:D] = (key_split[3] + key_split[4]).to_i
+    list
   end
 end
