8dde6570f4196ca63015b564f636c870649c27c1
Kate Tester <katemorris@gmail.com>
2020-09-18 23:23:32 -0600

    Adds breakdown method for offset and tests.

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -10,4 +10,12 @@ class OffsetTest < Minitest::Test
 
     assert_equal 4, offset.value.length
   end
+
+  def test_it_can_break_up_offset_value
+    offset = Offset.new
+    offset.stubs(:value).returns('0267')
+
+    expected = { A: 0, B: 2, C: 6, D: 7 }
+    assert_equal expected, offset.breakdown
+  end
 end
