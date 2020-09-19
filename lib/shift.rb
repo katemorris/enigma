7e3bee6498ea46b6d724e6149b31829c10c43e4c
Kate Tester <katemorris@gmail.com>
2020-09-19 13:58:06 -0600

    Modifies intialize and test to include key and offset

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,5 +1,12 @@
+require './lib/key.rb'
+require './lib/offset.rb'
 class Shift
-  def initialize
+  def initialize(key = nil, date = nil)
+    @key = Key.new(key)
+    @offset = Offset.new(date)
+  end
+
+  def method_name
 
   end
 end
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -6,7 +6,13 @@ require './lib/shift.rb'
 class ShiftTest < Minitest::Test
   def test_it_exists
     shift = Shift.new
-
     assert_instance_of Shift, shift
+    assert_includes 5, shift.key.value.length
+    assert_includes 4, shift.offset.value.length
+  end
+
+  def test_it_has_complete_shift_values
+    shift = Shift.new
+
   end
 end
