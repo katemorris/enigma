e4f2ba60ea98b2025be3c84702384906f35f6ca2
Kate Tester <katemorris@gmail.com>
2020-09-19 15:07:52 -0600

    Adds test for key check method.

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -1,18 +1,35 @@
 require './test/test_helper'
-require './lib/key.rb'
-require './lib/offset.rb'
+# require './lib/key.rb'
+# require './lib/offset.rb'
 require './lib/shift.rb'
 
 class ShiftTest < Minitest::Test
   def test_it_exists
     shift = Shift.new
-    assert_instance_of Shift, shift
-    assert_includes 5, shift.key.value.length
-    assert_includes 4, shift.offset.value.length
+    assert_equal 5, shift.key.value.length
+    assert_equal 4, shift.offset.value.length
+  end
+
+  def test_it_can_take_in_values
+    shift2 = Shift.new('03782', '050585')
+    assert_equal '03782', shift2.key.value
+    assert_equal '050585', shift2.offset.date
+  end
+
+  def test_key_returns_new_value_or_given
+    shift = Shift.new
+
+    assert_instance_of Key, shift.key_check(nil)
+    assert_instance_of Key, shift.key_check('03782')
   end
 
   def test_it_has_complete_shift_values
+    skip
     shift = Shift.new
+    shift.key.stubs(:value).returns('19987')
+    shift.offset.stubs(:value).returns('0267')
 
+    expected = { A: 19, B: 101, C: 104, D: 94 }
+    assert_equal expected, shift.breakdown
   end
 end
