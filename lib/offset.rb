0a09011875b2f1933979683cd0643d52f8d1b9ae
Kate Tester <katemorris@gmail.com>
2020-09-19 13:26:03 -0600

    Updates offset to take in date value at initialize

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,11 +1,16 @@
 require 'date'
 
 class Offset
+  def initialize(date = Date.today)
+    @date = date
+  end
+
+  def date_string(date)
+    date.strftime('%m%d%Y')
+  end
+
   def value
-    date = Date.today
-    date_string = date.strftime('%m%d%Y')
-    square_string = (date_string.to_i ** 2).to_s
-    square_string.split('').last(4).join
+    (date_string(@date).to_i ** 2).to_s.split('').last(4).join
   end
 
   def breakdown
