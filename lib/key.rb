933a9ab5b5432dc864674fc608d26548725ee418
Kate Tester <katemorris@gmail.com>
2020-09-19 14:34:20 -0600

    Modifies how breakdown gets value

--- lib/key.rb
+++ lib/key.rb
@@ -1,12 +1,12 @@
 class Key
   attr_reader :value
-  
+
   def initialize(key = 5.times.map { rand(10) }.join.to_s)
     @value = key
   end
 
   def breakdown
-    key_split = value.split("")
+    key_split = @value.split("")
     list = {}
     list[:A] = (key_split[0] + key_split[1]).to_i
     list[:B] = (key_split[1] + key_split[2]).to_i
