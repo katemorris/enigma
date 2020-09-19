214aa3cedbb32f37878743421a4ffc28b93e223e
Kate Tester <katemorris@gmail.com>
2020-09-19 16:19:02 -0600

    Adds encryption test.

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -3,10 +3,15 @@ require 'date'
 require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
-  def test_it_exists
+  def test_it_can_encrypt
     enigma = Enigma.new
 
-    assert_instance_of Enigma, enigma
+    expected = {
+                  encryption: "keder ohulw",
+                  key: "02715",
+                  date: "040895"
+                }
+    assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
   end
 
 end
