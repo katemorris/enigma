78a8996ffa43a0d114fa767efc4b3675bd17ac96
Kate Tester <katemorris@gmail.com>
2020-09-18 23:20:39 -0600

    Updates test to use string return on value.

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -1,12 +1,13 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/offset.rb'
+
 require 'mocha/minitest'
 
 class OffsetTest < Minitest::Test
   def test_it_exists
     offset = Offset.new
 
-    assert_equal 4, offset.value.to_s.length
+    assert_equal 4, offset.value.length
   end
 end
