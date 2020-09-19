6d74c81e584b736efdd67fae3a4f5b7d0cb82fe9
Kate Tester <katemorris@gmail.com>
2020-09-19 15:08:32 -0600

    Adds methods to check and reture correct key and offset object.

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,9 +1,26 @@
 require './lib/key.rb'
 require './lib/offset.rb'
 class Shift
+  attr_reader :key, :offset
   def initialize(key = nil, date = nil)
-    @key = Key.new(key)
-    @offset = Offset.new(date)
+    key_check(key)
+    offset_check(date)
+  end
+
+  def key_check(key)
+    if key.nil?
+      @key = Key.new
+    else
+      @key = Key.new(key)
+    end
+  end
+
+  def offset_check(date)
+    if date.nil?
+      @offset = Offset.new
+    else
+      @offset = Offset.new(date)
+    end
   end
 
   def method_name
