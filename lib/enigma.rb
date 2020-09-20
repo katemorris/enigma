70088a00d2e94d4ae3315d599995c9c5c14f25f3
Kate Tester <katemorris@gmail.com>
2020-09-19 21:43:59 -0600

    Adds decryption methods.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,7 @@
 require './lib/shift'
 class Enigma
   @@chars = ("a".."z").to_a << " "
+
   def line_breakdown(string)
     string.split('')
   end
@@ -46,5 +47,46 @@ class Enigma
       key: key,
       date: date
     }
+  end
+
+  def decrypt(string, key, date)
+    {
+      decryption: decrypt_string(string, key, date),
+      key: key,
+      date: date
+    }
+  end
+
+  # Unchanged
+  def decrypt_string(string, key, date)
+    round = 0
+    line_breakdown(string).map do |char|
+      if @@chars.include?(char)
+        round += 1
+        unshift_letter(char, round, key, date)
+      else
+        char = char
+      end
+    end.join
+  end
+
+  #only Rotate method is changed
+  def unshift_letter(char, round, key, date)
+    if round <= 4
+      move = get_shift(key, date).values[round - 1]
+      unrotate(move, char)
+    else
+      move = get_shift(key, date).values[(round % 4) - 1]
+      unrotate(move, char)
+    end
+  end
+
+  def unrotate(move, char)
+    new = @@chars.index(char) - move
+    if new < 0
+      @@chars[27 + new]
+    else
+      @@chars[new]
+    end
   end
 end
