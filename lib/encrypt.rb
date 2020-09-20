a9ef9c7c7dee9fb81159688609b7928591fff72e
Kate Tester <katemorris@gmail.com>
2020-09-20 12:44:54 -0600

    Adds runner file for encrypt.

---
+++ lib/encrypt.rb
@@ -0,0 +1,14 @@
+require './lib/enigma.rb'
+enigma = Enigma.new
+
+contents = open(ARGV.first).readlines
+key = ''
+date = ''
+encrypted_message = contents.map do |line|
+  array = enigma.encrypt(line)
+  date = array[:date]
+  key = array[:key]
+  array[:encryption]
+end
+encrypted = open(ARGV.last, 'w').write(encrypted_message.join)
+puts "Created #{ARGV.last} with the key #{key} and date #{date}"
