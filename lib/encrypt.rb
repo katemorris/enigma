a6f4895add06f882056bac15240d0e24b3f8a0ff
Kate Tester <katemorris@gmail.com>
2020-09-20 13:23:40 -0600

    Adds conditional to deal with single key across all lines.

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -5,10 +5,14 @@ contents = open(ARGV.first).readlines
 key = ''
 date = ''
 encrypted_message = contents.map do |line|
-  array = enigma.encrypt(line)
-  date = array[:date]
-  key = array[:key]
-  array[:encryption]
+  if date == '' && key == ''
+    array = enigma.encrypt(line)
+    date = array[:date]
+    key = array[:key]
+    array[:encryption]
+  else
+    enigma.encrypt(line, key, date)[:encryption]
+  end
 end
 open(ARGV.last, 'w').write(encrypted_message.join)
 puts "Created #{ARGV.last} with the key #{key} and date #{date}"
