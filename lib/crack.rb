bcc20470fd92a36d1a85f42b3d5ddb28139e3c1d
Kate Tester <katemorris@gmail.com>
2020-09-21 20:40:17 -0600

    Trailing space issue I keep forgetting!

--- lib/crack.rb
+++ lib/crack.rb
@@ -5,12 +5,12 @@ contents = open(ARGV.first).readlines
 key = ''
 date = ARGV.last
 if contents.count > 1
-  key = enigma.crack(contents.last, date)[:key]
+  key = enigma.crack(contents.last.strip, date)[:key]
   decrypted_message = contents.map do |line|
     enigma.decrypt(line, key, date)[:decryption]
   end.join
 else
-  crack = enigma.crack(contents.first, date)
+  crack = enigma.crack(contents.first.strip, date)
   key = crack[:key]
   decrypted_message = crack[:decryption]
 end
