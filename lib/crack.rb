9de67a0eecbe8e5a66e99d05f435303205213e80
Kate Tester <katemorris@gmail.com>
2020-09-21 20:28:33 -0600

    Quick fix to cracked to move join for multiline.

--- lib/crack.rb
+++ lib/crack.rb
@@ -8,11 +8,11 @@ if contents.count > 1
   key = enigma.crack(contents.last, date)[:key]
   decrypted_message = contents.map do |line|
     enigma.decrypt(line, key, date)[:decryption]
-  end
+  end.join
 else
-  crack = enigma.crack(contents.first, date)[:key]
+  crack = enigma.crack(contents.first, date)
   key = crack[:key]
   decrypted_message = crack[:decryption]
 end
-open(ARGV[1], 'w').write(decrypted_message.join)
+open(ARGV[1], 'w').write(decrypted_message)
 puts "Created #{ARGV[1]} with the cracked key #{key} and date #{date}"
